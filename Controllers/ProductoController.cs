b5edaa97d8ae750996d1542c0161a1a1cacab502
Diego <diegosebastian_t@hotmail.com>
2023-10-04 19:09:56 -0500

    Add project files.

---
+++ Controllers/ProductoController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Ejemplo1.Utils;
+using Ejemplo1.Models;
+
+namespace Ejemplo1.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: ProductoController
+        public IActionResult Index()
+        {
+           
+            return View(Utils.Utils.ListaProductos);
+        }
+
+        // GET: ProductoController/Details/5
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductoController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] // Etiqueta propia de ASP .NET
+        public IActionResult Create(Producto producto) //Aquí recibo el objeto de tipo producto
+        {
+            if (producto != null)
+            {
+                int i = Utils.Utils.ListaProductos.Count() + 1;
+                producto.IdProducto = i;
+                Utils.Utils.ListaProductos.Add(producto); // Agrego el producto a la lista
+
+            }
+            
+            return RedirectToAction("Index");
+        }
+
+
+        // GET: ProductoController/Edit/5
+        public IActionResult Edit(int IdProducto)
+        {
+
+            // Busco el producto mediante una función anónima
+            Producto producto = Utils.Utils.ListaProductos.Find(ObjetoActual => ObjetoActual.IdProducto == IdProducto);
+            if (producto != null)
+            {
+
+                // Retorno el producto a la vista
+                return View(producto);
+
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Producto producto)
+        {
+            // Busco el producto mediante una función anónima
+            Producto producto2 = Utils.Utils.ListaProductos.Find(ObjetoActual => ObjetoActual.IdProducto == producto.IdProducto);
+
+            if (producto2 != null)
+            {
+
+                producto2.Nombre = producto.Nombre;
+                producto2.Descripcion = producto.Descripcion;
+                producto2.Cantidad = producto.Cantidad;    
+
+                return RedirectToAction("Index");
+
+            }
+            // Retorno el producto a la vista
+            return View();
+        }
+
+
+
+        // GET: ProductoController/Delete/5
+        public IActionResult Delete(int IdProducto)
+        {
+            // Busco el producto mediante una función anónima
+            Producto producto2 = Utils.Utils.ListaProductos.Find(ObjetoActual => ObjetoActual.IdProducto == IdProducto);
+
+            if (producto2 != null)
+            {
+
+                Utils.Utils.ListaProductos.Remove(producto2);
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+
+     
+       
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,26 @@
+﻿namespace Ejemplo1.Models
+{
+    public class Producto
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Cantidad { get; set; }
+
+        //public Producto(int IdProducto, string Nombre, string Descripcion, int cantidad)
+        //{
+        //    this.IdProducto = IdProducto;
+        //    this.Nombre = Nombre;
+        //    this.Descripcion = Descripcion;
+        //    this.Cantidad = cantidad;
+        //}
+
+        // Constructor de tipado
+
+        // Constructor para trabajar con Blazor
+        //public Producto()
+        //{
+        //    this.IdProducto = Contador++;
+        //}
+    }
+}
